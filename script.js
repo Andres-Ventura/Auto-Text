98dd78ae39fada2939a7690cdbe3ce7a7b38c068
Andres Samuel Ventura jr <andresventura@MacBook-Pro.home>
2021-05-24 15:18:55 -0700

    Auto text animation

---
+++ script.js
@@ -0,0 +1,32 @@
+const textElement = document.getElementById('text')
+
+const speedElement = document.getElementById('speed')
+
+const text = 'Programming is fun!'
+
+let idx = 1
+
+let speed = 300 / speedElement.value
+
+writeText()
+
+function writeText() {
+    textElement.innerText = text.slice(0, idx)
+
+    idx++
+
+    if (idx > text.length) {
+        idx = 1
+    }
+
+    setTimeout(writeText, speed)
+}
+
+speedElement.addEventListener('input', (e) => {
+    speed = 300 / e.target.value
+    
+    if (speed < 1) {
+        speed = 1;
+        console.log("you cant do that")
+    }
+})
